37b5268dc2e453f00bc420446189b53d32ec0656
alot <alothdermawan@gmail.com>
2018-12-24 09:34:38 +0700

    Splash Home

---
+++ app/src/main/java/muaraenimkab/bps/go/id/bps/SplashScreenActivity.java
@@ -0,0 +1,40 @@
+package muaraenimkab.bps.go.id.bps;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    Thread splashTread;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        splashTread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    int waited = 0;
+                    while (waited < 1500) {
+                        sleep(100);
+                        waited += 100;
+                    }
+
+                    Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class)
+                            .addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    finish();
+                }
+            }
+        };
+        splashTread.start();
+
+    }
+}
