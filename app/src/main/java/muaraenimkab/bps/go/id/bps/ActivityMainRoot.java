31521c3ad32619ac13408d619deca052c7a04cf4
alot <alothdermawan@gmail.com>
2019-01-02 17:52:59 +0700

    Splash Home

--- app/src/main/java/muaraenimkab/bps/go/id/bps/ActivityMainRoot.java
+++ app/src/main/java/muaraenimkab/bps/go/id/bps/ActivityMainRoot.java
@@ -24,52 +24,140 @@ public class ActivityMainRoot extends AppCompatActivity {
 
         String flag = getIntent().getStringExtra("flag");
 
-        if (flag.equals("1")){
-            ActivityMainRoot.this.setTitle("Sosial dan Kependudukan");
+        if (flag.equals("0")) {
+            ActivityMainRoot.this.setTitle("Keadaan Geogradis & Iklim");
             data = new Models[]{
-                    new Models("Gender"),
-                    new Models("Geografi"),
-                    new Models("Iklim"),
-                    new Models("Indeks Pembangunan Manusia"),
-                    new Models("Kemiskinan dan Ketimpangan"),
-                    new Models("Kependudukan"),
-                    new Models("Kesehatan"),
-                    new Models("Konsumsi dan Pengeluaran"),
-                    new Models("Lingkungan Hidup"),
-                    new Models("Pemerintahan"),
-                    new Models("Pendidikan"),
-                    new Models("Perumahan"),
-                    new Models("Politik dan Keamanan"),
-                    new Models("Tenaga Kerja"),
+                    new Models("Letak Geografis & Kondisi Topografi"),
+                    new Models("Ketinggian"),
+                    new Models("Kemiringan Tanah"),
+                    new Models("Jenis Tanah"),
+                    new Models("Jarak ke Ibu Kota Kecamatan"),
+                    new Models("Keadaan Iklim"),
             };
-        }else if (flag.equals("2")){
-            ActivityMainRoot.this.setTitle("Ekonomi dan Perdagangan");
+        }else if (flag.equals("1")){
+            ActivityMainRoot.this.setTitle("Pemerintahan");
             data = new Models[]{
-                    new Models("Eksport-Import"),
-                    new Models("Energi"),
-                    new Models("Indeks Tendensi Konsumen"),
-                    new Models("Inflasi"),
-                    new Models("Keuangan"),
-                    new Models("Komunikasi"),
-                    new Models("Konstruksi"),
-                    new Models("Nilai Tukar Petani"),
-                    new Models("PDRB(Lapangan Usaha)"),
-                    new Models("PDRB(Pengeluaran)"),
-                    new Models("Pariwisata"),
-                    new Models("Produk Domestik Regional Bruto"),
+                    new Models("Wilayah Administrasi"),
+                    new Models("Keanggotaan Dewan"),
+                    new Models("Keadaan Pegawai Negeri Sipil"),
+            };
+//            data = new Models[]{
+//                    new Models("Gender"),
+//                    new Models("Geografi"),
+//                    new Models("Iklim"),
+//                    new Models("Indeks Pembangunan Manusia"),
+//                    new Models("Kemiskinan dan Ketimpangan"),
+//                    new Models("Kependudukan"),
+//                    new Models("Kesehatan"),
+//                    new Models("Konsumsi dan Pengeluaran"),
+//                    new Models("Lingkungan Hidup"),
+//                    new Models("Pemerintahan"),
+//                    new Models("Pendidikan"),
+//                    new Models("Perumahan"),
+//                    new Models("Politik dan Keamanan"),
+//                    new Models("Tenaga Kerja"),
+//            };
+        }else if (flag.equals("2")){
+            ActivityMainRoot.this.setTitle("Penduduk & Tenaga Kerja");
+            data = new Models[]{
+                    new Models("Penduduk"),
+                    new Models("Ketenagakerjaan"),
+//                    new Models("Eksport-Import"),
+//                    new Models("Energi"),
+//                    new Models("Indeks Tendensi Konsumen"),
+//                    new Models("Inflasi"),
+//                    new Models("Keuangan"),
+//                    new Models("Komunikasi"),
+//                    new Models("Konstruksi"),
+//                    new Models("Nilai Tukar Petani"),
+//                    new Models("PDRB(Lapangan Usaha)"),
+//                    new Models("PDRB(Pengeluaran)"),
+//                    new Models("Pariwisata"),
+//                    new Models("Produk Domestik Regional Bruto"),
             };
         }else if (flag.equals("3")){
-            ActivityMainRoot.this.setTitle("Pertanian dan Pertambangan");
+            ActivityMainRoot.this.setTitle("Sosial");
             data = new Models[]{
-                    new Models("Hortikultura"),
-                    new Models("Kehutanan"),
-                    new Models("Perikanan"),
-                    new Models("Perkebunan"),
-                    new Models("Pertambangan"),
-                    new Models("Peternakan"),
-                    new Models("Tanaman Pangan"),
+                    new Models("Pendidikan"),
+                    new Models("Kesehatan"),
+                    new Models("Keluarga Berencana"),
+                    new Models("Agama"),
+//                    new Models("Hortikultura"),
+//                    new Models("Kehutanan"),
+//                    new Models("Perikanan"),
+//                    new Models("Perkebunan"),
+//                    new Models("Pertambangan"),
+//                    new Models("Peternakan"),
+//                    new Models("Tanaman Pangan"),
             };
         } else if (flag.equals("4")){
+            ActivityMainRoot.this.setTitle("Pertanian");
+            data = new Models[]{
+                    new Models("Tanaman Pangan"),
+                    new Models("Perkebunan"),
+                    new Models("Peternakan"),
+                    new Models("Perikanan"),
+//                    new Models("Angka Harapan Hidup 2017 : 65,14 Tahun"),
+//                    new Models("Angka Partisipasi Murni SD 2017 : 78,83 Persen"),
+//                    new Models("Angka Partisipasi Murni SMP 2017 : 56,13 Persen"),
+//                    new Models("Angka Partisipasi Murni SMA 2017 : 43,48 Persen"),
+//                    new Models("Garis Kemiskinan September 2017 : 464.056 Rupiah/Kapita/Bulan"),
+            };
+        }else if (flag.equals("5")){
+            ActivityMainRoot.this.setTitle("Industri, Tambang, Energi");
+            data = new Models[]{
+                    new Models("Perindustrian"),
+                    new Models("Pertambangan"),
+                    new Models("Listrik"),
+                    new Models("Air"),
+            };
+        }else if (flag.equals("6")){
+            ActivityMainRoot.this.setTitle("Perdagangan & Konstruksi");
+            data = new Models[]{
+                    new Models("Perdagangan"),
+                    new Models("Konstruksi"),
+            };
+        }else if (flag.equals("7")){
+            ActivityMainRoot.this.setTitle("Transportasi, Komunikasi, Pariwisata");
+            data = new Models[]{
+                    new Models("Prasarana Jalan"),
+                    new Models("Angkutan Darat"),
+                    new Models("Pos"),
+                    new Models("Komunikasi"),
+                    new Models("Pariwisata"),
+            };
+        }else if (flag.equals("8")){
+            ActivityMainRoot.this.setTitle("Keuangan & Harga");
+            data = new Models[]{
+                    new Models("Keuangan"),
+                    new Models("Harga"),
+            };
+        }else if (flag.equals("9")){
+            ActivityMainRoot.this.setTitle("Pengeluaran & Konsumsi Penduduk");
+            data = new Models[]{
+                    new Models("Pengeluaran dan Konsumsi"),
+            };
+        }else if (flag.equals("10")){
+            ActivityMainRoot.this.setTitle("Pendapatan Regional");
+            data = new Models[]{
+                    new Models("Produk Domestik Regional Bruto (PDRB)"),
+                    new Models("Struktur Ekonomi"),
+                    new Models("Pertumbuhan Ekonomi"),
+                    new Models("Laju Inflasi PDRB"),
+                    new Models("Pendapatan per Kapita"),
+            };
+        }else if (flag.equals("11")){
+            ActivityMainRoot.this.setTitle("Kemiskinan");
+            data = new Models[]{
+                    new Models("Kemiskinan"),
+            };
+        }else if (flag.equals("12")){
+            ActivityMainRoot.this.setTitle("Perbandingan Regional");
+            data = new Models[]{
+                    new Models("Kependudukan"),
+                    new Models("Kemiskinan")
+            };
+        }else if (flag.equals("13")){
             ActivityMainRoot.this.setTitle("Indikator Strategis");
             data = new Models[]{
                     new Models("Angka Harapan Hidup 2017 : 65,14 Tahun"),
@@ -82,7 +170,7 @@ public class ActivityMainRoot extends AppCompatActivity {
 
         aList = new ArrayList<>(Arrays.asList(data));
 
-        if (flag.equals("4")){
+        if (flag.equals("13")){
             rView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
             linearLayoutManager = new LinearLayoutManager(this);
             rView.setLayoutManager(linearLayoutManager);
--- app/src/main/java/muaraenimkab/bps/go/id/bps/MainActivity.java
+++ app/src/main/java/muaraenimkab/bps/go/id/bps/MainActivity.java
@@ -4,104 +4,166 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-    CardView cvSosial, cvEkonomi, cvPertanian, cvIndikator, cvPublikasi, cvBeritaStatistik, cvBerita, cvKontak, cvTentang;
+//    CardView cvSosial, cvEkonomi, cvPertanian, cvIndikator, cvPublikasi, cvBeritaStatistik, cvBerita, cvKontak, cvTentang;
+
+    RecyclerView rView;
+    ArrayList<Models> aList;
+    Models[] data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        cvSosial = findViewById(R.id.card_view_sosial);
-        cvEkonomi = findViewById(R.id.card_view_ekonomi);
-        cvPertanian = findViewById(R.id.card_view_pertanian);
-        cvIndikator = findViewById(R.id.card_view_indikator);
-        cvPublikasi = findViewById(R.id.card_view_publikasi);
-        cvBeritaStatistik = findViewById(R.id.card_view_berita_statistik);
-        cvBerita = findViewById(R.id.card_view_berita);
-        cvKontak = findViewById(R.id.card_view_kontak);
-        cvTentang = findViewById(R.id.card_view_tentang);
 
-        cvSosial.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ActivityMainRoot.class);
-                intent.putExtra("flag", "1");
-                startActivity(intent);
-            }
-        });
+        rView = findViewById(R.id.recycler_view);
 
-        cvEkonomi.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ActivityMainRoot.class);
-                intent.putExtra("flag", "2");
-                startActivity(intent);
-            }
-        });
+        ImageView imageView = findViewById(R.id.image_view);
 
-        cvPertanian.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ActivityMainRoot.class);
-                intent.putExtra("flag", "3");
-                startActivity(intent);
-            }
-        });
+//        cvSosial = findViewById(R.id.card_view_sosial);
+//        cvEkonomi = findViewById(R.id.card_view_ekonomi);
+//        cvPertanian = findViewById(R.id.card_view_pertanian);
+//        cvIndikator = findViewById(R.id.card_view_indikator);
+//        cvPublikasi = findViewById(R.id.card_view_publikasi);
+//        cvBeritaStatistik = findViewById(R.id.card_view_berita_statistik);
+//        cvBerita = findViewById(R.id.card_view_berita);
+//        cvKontak = findViewById(R.id.card_view_kontak);
+//        cvTentang = findViewById(R.id.card_view_tentang);
+//
+//        cvSosial.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, ActivityMainRoot.class);
+//                intent.putExtra("flag", "1");
+//                startActivity(intent);
+//            }
+//        });
+//
+//        cvEkonomi.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, ActivityMainRoot.class);
+//                intent.putExtra("flag", "2");
+//                startActivity(intent);
+//            }
+//        });
+//
+//        cvPertanian.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, ActivityMainRoot.class);
+//                intent.putExtra("flag", "3");
+//                startActivity(intent);
+//            }
+//        });
+//
+//        cvIndikator.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, ActivityMainRoot.class);
+//                intent.putExtra("flag", "4");
+//                startActivity(intent);
+//            }
+//        });
+//
+//        cvPublikasi.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, ActivityPublikasi.class);
+//                intent.putExtra("flag", "5");
+//                startActivity(intent);
+//            }
+//        });
+//
+//        cvBerita.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, ActivityBerita.class);
+//                intent.putExtra("flag", "7");
+//                startActivity(intent);
+//            }
+//        });
+//
+//        cvBeritaStatistik.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, ActivityBeritaResmi.class);
+//                intent.putExtra("flag", "6");
+//                startActivity(intent);
+//            }
+//        });
+//
+//        cvKontak.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, ActivityKontak.class);
+//                intent.putExtra("flag", "8");
+//                startActivity(intent);
+//            }
+//        });
+//
+//        cvTentang.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, ActivityTentang.class);
+//                intent.putExtra("flag", "9");
+//                startActivity(intent);
+//            }
+//        });
 
-        cvIndikator.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ActivityMainRoot.class);
-                intent.putExtra("flag", "4");
-                startActivity(intent);
-            }
-        });
+        data = new Models[]{
+                new Models("Keadaan Geogradis & Iklim"),
+                new Models("Pemerintahan"),
+                new Models("Penduduk & Tenaga Kerja"),
+                new Models("Sosial"),
+                new Models("Pertanian"),
+                new Models("Industri, Tambang, Energi"),
+                new Models("Perdagangan & Konstruksi"),
+                new Models("Transportasi, Komunikasi, Pariwisata"),
+                new Models("Keuangan & Harga"),
+                new Models("Pengeluaran & Konsumsi Penduduk"),
+                new Models("Pendapatan Regional"),
+                new Models("Kemiskinan"),
+                new Models("Perbandingan Regional"),
+                new Models("Indikator Strategis"),
+                new Models("Publikasi Statistik"),
+                new Models("Berita Resmi Statistik"),
+                new Models("Berita"),
+                new Models("Kontak"),
+                new Models("Tentang Kami")
+        };
 
-        cvPublikasi.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ActivityPublikasi.class);
-                intent.putExtra("flag", "5");
-                startActivity(intent);
-            }
-        });
+        aList = new ArrayList<>(Arrays.asList(data));
 
-        cvBerita.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ActivityBerita.class);
-                intent.putExtra("flag", "7");
-                startActivity(intent);
-            }
-        });
+        DisplayMetrics displayMetrics = this.getResources().getDisplayMetrics();
+        float dpWidth = displayMetrics.widthPixels / displayMetrics.density;
+        int noOfColumns = (int) (dpWidth / 120);
 
-        cvBeritaStatistik.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ActivityBeritaResmi.class);
-                intent.putExtra("flag", "6");
-                startActivity(intent);
-            }
-        });
+        imageView.setLayoutParams(new LinearLayout.LayoutParams(displayMetrics.widthPixels/1, displayMetrics.widthPixels/2));
 
-        cvKontak.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ActivityKontak.class);
-                intent.putExtra("flag", "8");
-                startActivity(intent);
-            }
-        });
+        rView.setHasFixedSize(true);
+        rView.setLayoutManager(new GridLayoutManager(this, noOfColumns));
+        MenuViewAdapter rcAdapter = new MenuViewAdapter(this, aList);
+        rView.setAdapter(rcAdapter);
 
-        cvTentang.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ActivityTentang.class);
-                intent.putExtra("flag", "9");
-                startActivity(intent);
-            }
-        });
+//        rView.setNestedScrollingEnabled(false);
+//        rView.setLayoutManager(new GridLayoutManager(this, 1, GridLayoutManager.HORIZONTAL, false));
+//        rView.setItemAnimator(new DefaultItemAnimator());
+//        MenuViewAdapter rcAdapter = new MenuViewAdapter(this, aList);
+//        rView.setAdapter(rcAdapter);
+
     }
 }
